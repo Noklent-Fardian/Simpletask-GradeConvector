ab4658f119521a851b9161d6152494b79a4b0ee9
Noklent <noklen350@gmail.com>
2022-04-08 08:32:18 +0700

    first commit

---
+++ 6april22/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.label_rata = New System.Windows.Forms.Label()
+        Me.label_grade = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(361, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(195, 29)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Grade Convector"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(118, 127)
+        Me.TextBox1.MaxLength = 3
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(101, 30)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(144, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 29)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "IPA"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(275, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(146, 29)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Bahasa Indo"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(293, 127)
+        Me.TextBox2.MaxLength = 3
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(101, 30)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(460, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(172, 29)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Bahasa Inggris"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(487, 127)
+        Me.TextBox3.MaxLength = 3
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(101, 30)
+        Me.TextBox3.TabIndex = 7
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(683, 68)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(136, 29)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Matematika"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4.Location = New System.Drawing.Point(697, 127)
+        Me.TextBox4.MaxLength = 3
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(101, 30)
+        Me.TextBox4.TabIndex = 9
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(113, 221)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(129, 29)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Rata Rata: "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(113, 270)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(86, 29)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Grade:"
+        '
+        'label_rata
+        '
+        Me.label_rata.AutoSize = True
+        Me.label_rata.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label_rata.Location = New System.Drawing.Point(265, 221)
+        Me.label_rata.Name = "label_rata"
+        Me.label_rata.Size = New System.Drawing.Size(0, 29)
+        Me.label_rata.TabIndex = 13
+        '
+        'label_grade
+        '
+        Me.label_grade.AutoSize = True
+        Me.label_grade.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label_grade.Location = New System.Drawing.Point(242, 270)
+        Me.label_grade.Name = "label_grade"
+        Me.label_grade.Size = New System.Drawing.Size(0, 29)
+        Me.label_grade.TabIndex = 14
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(841, 121)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 36)
+        Me.Button1.TabIndex = 15
+        Me.Button1.Text = "Check"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(949, 540)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.label_grade)
+        Me.Controls.Add(Me.label_rata)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents label_rata As Label
+    Friend WithEvents label_grade As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ 6april22/Form1.vb
@@ -0,0 +1,104 @@
+﻿Public Class Form1
+    Dim mtk, bindo, bing, ipa, ratarata As Double
+
+    Sub rata()
+        ipa = Convert.ToDouble(TextBox1.Text)
+        bindo = Convert.ToDouble(TextBox2.Text)
+        bing = Convert.ToDouble(TextBox3.Text)
+        mtk = Convert.ToDouble(TextBox4.Text)
+        ratarata = (ipa + bindo + bing + mtk) / 4
+    End Sub
+    Private Sub TextBox1_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        Dim kunci As Short = Asc(e.KeyChar)
+        If (e.KeyChar Like "[0-9]" _
+            OrElse kunci = Keys.Back) Then
+            kunci = 0
+        End If
+        e.Handled = CBool(kunci)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or
+                TextBox2.Text = "" Or
+                TextBox3.Text = "" Or
+                TextBox4.Text = "" Then
+            MsgBox("Fill all columns", vbCritical)
+        ElseIf TextBox1.Text > 100 Then
+        MsgBox("Value for IPA Is Incorrect", vbCritical)
+                TextBox1.Text = Nothing
+            ElseIf TextBox2.Text > 100 Then
+                MsgBox("Value For Bindo is incorrect", vbCritical)
+                TextBox2.Text = Nothing
+            ElseIf TextBox3.Text > 100 Then
+                MsgBox("Value for Bing is incorrect", vbCritical)
+                TextBox3.Text = Nothing
+            ElseIf TextBox4.Text > 100 Then
+                MsgBox("Value for MTk is incorrect", vbCritical)
+                TextBox4.Text = Nothing
+            Else
+                Call rata()
+            If ratarata = 100 Then
+                MsgBox("You're S Grade", MsgBoxStyle.Information)
+                label_grade.Text = "S"
+            ElseIf ratarata > 100 Then
+                MsgBox("Input is incorrect", vbCritical)
+            ElseIf ratarata > 94 Then
+                MsgBox("You're A+ Grade", MsgBoxStyle.Information)
+                label_grade.Text = "A+"
+            ElseIf ratarata > 89 Then
+                MsgBox("You're A Grade", MsgBoxStyle.Information)
+                label_grade.Text = "A"
+            ElseIf ratarata > 84 Then
+                MsgBox("You're B+ Grade", MsgBoxStyle.Information)
+                label_grade.Text = "B+"
+            ElseIf ratarata > 79 Then
+                MsgBox("You're B Grade", MsgBoxStyle.Information)
+                label_grade.Text = "B"
+            ElseIf ratarata > 74 Then
+                MsgBox("You're C+ Grade", MsgBoxStyle.Information)
+                label_grade.Text = "C+"
+            ElseIf ratarata > 69 Then
+                MsgBox("You're C Grade", MsgBoxStyle.Information)
+                label_grade.Text = "C"
+            ElseIf ratarata < 69 Then
+                MsgBox("You're D Grade", MsgBoxStyle.Information)
+                label_grade.Text = "D"
+            Else
+                MsgBox("Input is incorrect", vbCritical)
+            End If
+        End If
+        label_rata.Text = CStr(ratarata)
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TextBox2.KeyPress
+        Dim kunci As Short = Asc(e.KeyChar)
+        If (e.KeyChar Like "[0-9]" _
+            OrElse kunci = Keys.Back) Then
+            kunci = 0
+        End If
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TextBox3.KeyPress
+        Dim kunci As Short = Asc(e.KeyChar)
+        If (e.KeyChar Like "[0-9]" _
+            OrElse kunci = Keys.Back) Then
+            kunci = 0
+        End If
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As KeyPressEventArgs) Handles TextBox4.KeyPress
+        Dim kunci As Short = Asc(e.KeyChar)
+        If (e.KeyChar Like "[0-9]" _
+            OrElse kunci = Keys.Back) Then
+            kunci = 0
+        End If
+
+    End Sub
+End Class
